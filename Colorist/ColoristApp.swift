2fbf42f096d51b8386057e542a28f2f4ea5fb1f7
hossein shademani <hossein.sh93@gmail.com>
2021-09-28 18:27:37 +0330

    Initial Commit

---
+++ Colorist/ColoristApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoristApp.swift
+//  Colorist
+//
+//  Created by sourena shadmahani on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ColoristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colorist
+//
+//  Created by sourena shadmahani on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
